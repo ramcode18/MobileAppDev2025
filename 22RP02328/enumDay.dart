d839aaeaf2849e12bd3ecf54ea7df0ccfeae1c1d
ramcode18 <rambertshimwe18@gmail.com>
2025-06-10 11:23:20 +0200

    today commit

---
+++ 22RP02328/enumDay.dart
@@ -0,0 +1,29 @@
+enum day{
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday
+}
+
+enum status{
+  Leaerning,
+  Learning,
+  Relaxing
+
+}
+
+void main(){
+
+day myday =day.Friday;
+
+print('$myday');
+
+
+
+void greatUser(){
+  print("Hello , Dart");
+}
+greatUser();
+}
